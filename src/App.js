ef02903da4dbbe1a7faa7f4215035c6d31b75cc2
elywin <lukeeraelywin@gmail.com>
2021-06-09 02:13:07 +0300

    create react app

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { BrowserRouter,Switch, Route } from "react-router-dom";
+import Home from './components/Home';
+import Login from './components/Login';
+import './App.css';
+import PostList from './components/PostList';
+import Posts from './components/Posts';
+import Answers from './components/Answers';
+
+function App() {
+  return (
+    <div className="App">
+       <React.Fragment>
+       <BrowserRouter>
+          <Switch>
+            <Route exact path='/'> <Home /> </Route>
+            <Route path="/auth/login" > <Login /> </Route>
+            <Route path="/questions/:_id/:title"> <Answers /> </Route>
+            <Route path="/questions/ask" > <Posts /> </Route>
+            <Route path="/questions" > <PostList /> </Route>
+        </Switch>
+        </BrowserRouter>
+      </React.Fragment>
+    </div>
+  );
+}
+
+export default App;
